eaf635772eaf10c1d6e21d49bde11ce587f63ed1
Daniel Studencki <daniel.studencki@gmail.com>
2020-01-30 14:39:21 +0100

    Resolve type errors.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Highcharts from "highcharts";
 import Component from "vue-class-component";
 
 @Component
@@ -15,39 +16,46 @@ export default class OverTimeChart extends Vue {
   results: number[] = [1, 2, 3, 4, 5];
 
   get performanceTimeline() {
-    var ctx = this;
-    return {
-      rangeSelector: {
-        selected: 1
-      },
-      title: {
-        text: "Performance Over Time"
-      },
-      yAxis: {
-        labels: {
-          formatter: function(): string {
-            let labels = ["a", "b", "c", "d", "e"];
-            let current: number = this.value;
-            return labels[current];
+    let ctx = this,
+      chartConfig: Highcharts.Options = {
+        rangeSelector: {
+          selected: 1
+        },
+        title: {
+          text: "Performance Over Time"
+        },
+        yAxis: {
+          labels: {
+            formatter: function(
+              this: Highcharts.AxisLabelsFormatterContextObject
+            ): string {
+              let labels = ["a", "b", "c", "d", "e"];
+              let current: number = this.value;
+              return labels[current];
+            }
           }
-        }
-      },
-      tooltip: {
-        formatter: function(): string {
-          return "<b>Status:</b> " + this.y;
-        }
-      },
-      series: [
-        {
-          name: "Status",
-          step: true,
-          data: this.results.map(test => {
-            return [1580315144 + 86000 * test * 1000, test];
-          }),
-          pointInterval: 1000 * 3600 * 24
-        }
-      ]
-    };
+        },
+        tooltip: {
+          formatter: function(
+            this: Highcharts.TooltipFormatterContextObject
+          ): string {
+            return "<b>Status:</b> " + this.y;
+          }
+        },
+        series: [
+          {
+            type: "line",
+            name: "Status",
+            step: "left",
+            data: this.results.map(test => {
+              return [1580315144 + 86000 * test * 1000, test];
+            }),
+            pointInterval: 1000 * 3600 * 24
+          }
+        ]
+      };
+
+    return chartConfig;
   }
 }
 </script>
